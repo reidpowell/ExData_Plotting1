e5c13ef20190a3e0ac3424151d77fe7f77d3f966
reidpowell <reidpowell@me.com>
2016-01-13 00:26:43 -0500

    Update plot1.R
    fix date

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 dat <- read.table("./household_power_consumption.txt", header = TRUE, sep = ";")
 
 ## Select the subset of data between 2007-02-01 and 2007-02-02
-subdat <- subset(dat, Date == "2/1/2007" | Date == "2/2/2007")
+subdat <- subset(dat, Date == "1/2/2007" | Date == "2/2/2007")
 
 ## Open the device
 png(filename="./plot1.png", width = 480, height = 480)
